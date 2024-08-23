396c55179e189b46190f2d6d5ab60bb8d61b4379
Lashawn Butler <lashawnbutler50@gmail.com>
2024-08-23 14:28:51 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: name,
+    email: email
+  })
+}
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        console.log(object)
+        document.body.append(object.id)
+    })
+    .catch(function (error) {
+        alert("ERROR");
+        console.log(error.message)
+        document.body.append(error.message)
+    })
+
+}
